b677f34f0b29d9ff2c2895ee0217a324249ab0b0
Angelyn Liem <angelynliem@gmail.com>
2026-09-21 18:01:04 -0400

    completed wave 1 and tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,28 @@
 class Vendor:
-    pass
+
+#Each `Vendor` will have an attribute named `inventory`, 
+#which is an empty list by default
+
+#When we instantiate an instance of `Vendor`, we can 
+#optionally pass in a list with the keyword argument `inventory`
+
+    def __init__(self, inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+        
+    
+    
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return None
+        self.inventory.remove(item)
+        return item
